26819de94babfa39d61f85e9106bccf9ef8794a2
Ernesto <jorge.cywdt@gmail.com>
2023-04-17 11:24:11 -0500

    Cambios realizados por SISTEMASUNI
    ---- Gitignore ----
    
    - Gitignore for a Django project
    https://djangowaves.com/tips-tricks/gitignore-for-a-django-project/
    
    - Permitimos SQLite3 en Gitignore

--- DjangoHindiTutorial/urls.py
+++ DjangoHindiTutorial/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for DjangoHindiTutorial project.
+"""DjangoHindiTutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DjangoHindiApp import views
+from django.conf.urls.static import static
+from DjangoHindiTutorial import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("demoPage",views.demoPage),
+    path("demoPage2",views.demoPage2),
+    path('',views.loginUser,name="login"),
+    path('signup',views.signupUser,name="signup"),
+    path('signup_process',views.signup_process,name="signup_process"),
+    path('login_proces',views.login_proces,name="login_proces"),
+    path('home',views.home,name="home"),
+    path('logout',views.logoutUser,name="logout"),
+    path('add_tasks',views.add_tasks,name="add_tasks"),
+    path('delete/<str:id>',views.delete_task,name="delete_task"),
+    path('edit_task/<str:id>',views.edit_task,name="edit_task"),
+    path('edit_tasks_save/<str:id>',views.edit_tasks_save,name="edit_tasks_save"),
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
